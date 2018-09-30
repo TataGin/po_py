1de2f128568a05a0f67354e46c331a52a59400c5
Florian Ginez <F.Ginez@c7plus.com>
2018-10-01 00:03:27 +0100

    Pep8 Correction

--- po_py/pyData/csv_tools.py
+++ po_py/pyData/csv_tools.py
@@ -7,8 +7,6 @@ def concat_csv(input_path, output_path=None, input_file='*',
                output_file='Concat.csv', index_col=[0],
                join='outer', subfolders=False,
                fill_na=False, ffill=None):
-
-
     ''' Concatenates all csv files in a folder and optionnally its subfolders
 
         Positional arguments:
@@ -25,42 +23,41 @@ def concat_csv(input_path, output_path=None, input_file='*',
 
     '''
 
-
-    #Adapt path to look in subfolders
+    # Adapt path to look in subfolders
     sub = _search_subfolders(subfolders)
 
-    #Add csv extension to input
+    # Add csv extension to input
     input_file = _has_csv_extension(input_file)
 
-    #Output path = input path if not precised by user
+    # Output path = input path if not precised by user
     output_path = _check_output_path(input_path, output_path)
 
-    #Check if file with output name already exists
+    # Check if file with output name already exists
     if file_exists(output_file, input_path, sub):
-        if not _yes_no('Output file already exists. Overwrite? [Y/N]')
-            output_file = _increment_file_name(output_file)
+        if not _yes_no('Output file already exists. Overwrite? [Y/N]'):
+        output_file = _increment_file_name(output_file)
 
-    #List of all paths to matching files
+    # List of all paths to matching files
     occurences = glob.glob(input_path+sub+input_file, recursive=True)
 
     print('{0} matching files found'.format(len(occurences)))
 
-    #Concatenate files with pandas
+    # Concatenate files with pandas
     df = pd.concat([pd.read_csv(f, index_col=index_col)
-                        for f in occurences], sort=False)
+                    for f in occurences], sort=False)
 
-    #Write result to CSV
+    # Write result to CSV
     df.to_csv(os.path.join(output_path, output_file))
 
-    print('All files concatenated in {0}'/
-            .format(os.path.join(output_path, output_file)))
+    print('All files concatenated in {0}' /
+          .format(os.path.join(output_path, output_file)))
 
 
 def _yes_no(question):
     answer = input(question + "(y/n): ").lower().strip()
     print("")
-    while not(answer == "y" or answer == "yes" or \
-    answer == "n" or answer == "no"):
+    while not(answer == "y" or answer == "yes" or
+              answer == "n" or answer == "no"):
         print("Input yes or no")
         answer = input(question + "(y/n):").lower().strip()
         print("")
@@ -76,9 +73,10 @@ def _search_subfolders(subfolders):
     else:
         return '/'
 
+
 def _has_csv_extension():
     if not input_file.endswith('.csv')
-        return input_file+'.csv'
+    return input_file+'.csv'
 
 
 def file_exists(output_file, directory, sub):
