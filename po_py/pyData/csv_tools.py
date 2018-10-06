1e9f4403a4cc62bb6037093ba5292911291a60f3
Florian Ginez <F.Ginez@c7plus.com>
2018-10-06 12:15:13 +0100

    Commit

--- po_py/pyData/csv_tools.py
+++ po_py/pyData/csv_tools.py
@@ -3,19 +3,19 @@ import glob
 import pandas as pd
 
 
-def concat_csv(input_path, output_path=None, input_file='*',
+def concat_csv(search_folder, output_folder=None, search_file='*',
                output_file='Concat.csv', index_col=[0],
                join='outer', subfolders=False,
                fill_na=False, ffill=None):
     ''' Concatenates all csv files in a folder and optionnally its subfolders
 
         Positional arguments:
-        :input_path str
+        :search_folder str
 
         Keyword arguments:
         :output_file str
         :index_col list
-        :output_path None
+        :output_folder None
         :join str
         :subfolders boolean
         :fillna boolean
@@ -23,34 +23,111 @@ def concat_csv(input_path, output_path=None, input_file='*',
 
     '''
 
+    # Add csv extension to input
+    search_file = _add_extension(search_file, 'csv')
+    output_file = _add_extension(output_file, 'csv')
+
+    # Output path = input path if not precised by user
+    output_folder = _if_none(output_folder, search_folder)
+
     # Adapt path to look in subfolders
     sub = _search_subfolders(subfolders)
 
-    # Add csv extension to input
-    input_file = _has_csv_extension(input_file)
-
-    # Output path = input path if not precised by user
-    output_path = _check_output_path(input_path, output_path)
-
     # Check if file with output name already exists
-    if file_exists(output_file, input_path, sub):
-        if not _yes_no('Output file already exists. Overwrite? [Y/N]'):
-        output_file = _increment_file_name(output_file)
+    _prompt_if_exists(output_file)
+    if file_exists(os.path.join(output_folder, output_file)):
+        output_file = _prompt_replace(output_file)
 
     # List of all paths to matching files
-    occurences = glob.glob(input_path+sub+input_file, recursive=True)
+    occurences = glob.glob(search_folder+sub+search_file, recursive=True)
 
-    print('{0} matching files found'.format(len(occurences)))
+    print('{0} matching files found'.format(len(occurences)+1))
 
     # Concatenate files with pandas
-    df = pd.concat([pd.read_csv(f, index_col=index_col)
-                    for f in occurences], sort=False)
+    for f in occurences:
+        print('File # {} is being concatenated')
+        df = pd.concat(pd.read_csv(f, index_col=index_col), sort=False)
 
     # Write result to CSV
-    df.to_csv(os.path.join(output_path, output_file))
+    df.to_csv(os.path.join(output_folder, output_file))
 
-    print('All files concatenated in {0}' /
-          .format(os.path.join(output_path, output_file)))
+    print('All files concatenated in {0}'
+          .format(os.path.join(output_folder, output_file)))
+
+
+def _add_extension(file, ext):
+    ''' !!! See how to accept file both as a list or a string '''
+
+    ext = _format_ext(ext)
+
+    if _has_extension(file, ext):
+        return file
+    else:
+        return file+ext.lower()
+
+
+def _format_ext(ext):
+
+    # Treat for user input '.fileext'
+    if not ext.startswith('.'):
+        return '.'+ext.lower()
+    else:
+        return ext.lower()
+
+
+def _has_extension(file_path, ext):
+    ''' Check if a file has a specific extension
+        Could be more elegant
+
+        !!! See how to accept file both as a list or a string
+
+    '''
+
+    ext = _format_ext(ext)
+
+    # Check if file name finishes by extension
+    if os.path.splitext(file_path) == ext:
+        return True
+    else:
+        return False
+
+
+def _if_none(x, y):
+
+    if x:
+        return x
+    else:
+        return y
+
+
+def _search_subfolders(subfolders):
+    ''' Returns substring for searching in subfolders
+
+        Positional arguments:
+        : subfolders boolean
+
+    '''
+
+    if subfolders:
+        return '/**/'
+    else:
+        return '/'
+
+
+def _prompt_replace(file_path):
+    pass
+
+
+def file_exists(path):
+    """ Checks if the files already exists in the folder.
+
+        Positional arguments:
+        :path str
+
+    """
+
+    if file_exists(file_path):
+        return os.path.isfile(file_path)
 
 
 def _yes_no(question):
@@ -67,30 +144,8 @@ def _yes_no(question):
         return False
 
 
-def _search_subfolders(subfolders):
-    if subfolders:
-        return '/**/'
-    else:
-        return '/'
-
-
-def _has_csv_extension():
-    if not input_file.endswith('.csv')
-    return input_file+'.csv'
-
-
-def file_exists(output_file, directory, sub):
-    if output_file in glob.glob(directory+sub+'/**/output_file',
-                                recursive=True):
-        return True
-    else:
-        return False
-
-
-def _check_output_path(input_path, output_path):
-
-    if not output_path:
-        output_path = input_path
+def _increment_file_name(file_path):
+    pass
 
 
 def _fill_na(df, fill_na, ffill):
@@ -99,7 +154,7 @@ def _fill_na(df, fill_na, ffill):
         df = df.fillna(value=fill_na, method=ffill)
 
 
-def excel_to_csv(input_path, output_path=None, skiprows=None):
+def excel_to_csv(input_path, output_folder=None, skiprows=None):
     ''' Saves a .csv copy of all Excel file (.xls or .xlsx) in a folder
         If no output path is given, saves the stacked file in the same
         folder as the input files
