91a9831b45c3834482ba3a8556187bbfa92ceb88
Florian Ginez <F.Ginez@c7plus.com>
2018-10-20 02:42:35 +0100

    added docstring for creaepath

--- po_py/pyData/path_tools.py
+++ po_py/pyData/path_tools.py
@@ -2,21 +2,26 @@ import os
 import pathlib
 
 
-def create_path(root, file, ext=None):
+def create_path(root, file, suffix=None):
     """ Creates a full path from user input folder, file_name
 
         Potential improvements:
             - Add treatment if user does not enter raw string
 
         Keyword arguments:
+        :root string
+        :file string
+
+        Positional arguments:
+        :suffix string
 
     """
 
     path = pathlib.Path(root) / file
 
-    if ext:
-        if not ext.startswith('.'):
-            ext = '.'+ext
-        return path.with_suffix(ext)
+    if suffix:
+        if not suffix.startswith('.'):
+            suffix = '.'+suffix
+        return path.with_suffix(suffix)
     else:
         return path
