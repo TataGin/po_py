75f35757401ea710bc5cd9ca45f336baabce7550
Florian Ginez <F.Ginez@c7plus.com>
2018-10-24 23:12:33 -0400

    Added functions: yes_no, custom_question, if_none

---
+++ po_py/pyData/input_tools.py
@@ -0,0 +1,114 @@
+"""Contains functions to control for user inputs."""
+
+import pathlib as pl
+import sys
+import re
+
+
+def yes_no(question):
+    """Check if user gave a valid answer to a yes/no question.
+
+    Positional argments:
+    :question str
+    """
+    # Add '?' if not done by user
+    question = question.strip()
+    if not question.endswith('?'):
+        question += '?'
+
+    answer = input(question + " (y/n): ").lower().strip()
+    print('')
+    while not(answer == "y" or answer == "yes" or
+              answer == "n" or answer == "no"):
+        print("Input yes or no please! \n")
+
+        answer = input(question + "(y/n):").lower().strip()
+
+    if answer[0] == "y":
+        return True
+    else:
+        return False
+
+
+def custom_question(question, answers):
+    """Check if user gave a valid answer to the question.
+
+    Positional arguments:
+    :question str
+    :answers dict
+    """
+    # Add '?' if not done by user
+    question = question.strip()
+    if not question.endswith('?'):
+        question += '?'
+
+    valid_long_answers = list(map(lambda x: x.strip().lower(),
+                                  list(answers.keys())))
+    valid_short_answers = list(map(lambda x: x.strip().lower(),
+                                   list(answers.values())))
+    str_valid_answers = ', '.join(map(str, valid_long_answers))
+
+    answer = input(question + ' (' + str_valid_answers + '): ').strip().lower()
+    print('')
+
+    while not (answer in list(map(str.lower,
+                                  valid_long_answers+valid_short_answers))):
+        print('Answer not valid. Valid answers: '+str_valid_answers+'\n')
+        answer = input(question + ' (' + str_valid_answers +
+                       '): ').strip().lower()
+
+    return answer
+
+
+def if_none(x, y):
+    """If x is None, return y.
+
+    Positional arguments:
+    : x any
+    : y any
+    """
+    if x:
+        return x
+    else:
+        return y
+
+
+def prompt_if_exists(path):
+    """Ask user for action of output file name already exists.
+
+    Positional arguments:
+    :path str
+    """
+    if type(path) == str:
+        path = pl.Path([path][0])
+    if path.is_file():
+        choice = custom_question('File already exists. What should we do',
+                                 {'Cancel': 'c', 'Owerwrite': 'o',
+                                  'Increment': 'i', 'Change': 'c'})
+        if choice in ('overwrite', 'o'):
+            return uniquify(path)
+        # elif choice in ('change', 'c'):
+        #    new_name = input('What new name?')
+        else:
+            sys.exit()
+    else:
+        return path
+
+
+def uniquify(path, sep='_'):
+    """Increment file name if exists.
+
+    Positional arguments:
+    :path
+    """
+    if type(path) == str:
+        path = pl.Path([path][0])
+    folder = path.parent
+    name = path.stem
+    suffix = ''.join(path.suffixes)
+    s = re.search(re.escape(sep) + '[0-9]+$', name)
+    if s:
+        return folder.joinpath(name[:s.start()] + str(int(name[s.start()+1:])
+                                                      + 1) + suffix)
+    else:
+        return folder.joinpath(name + sep + '01' + suffix)
