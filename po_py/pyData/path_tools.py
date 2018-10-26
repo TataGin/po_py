ed784bed8bfc3842a2ef95779db545876561d5a2
Florian Ginez <F.Ginez@c7plus.com>
2018-10-25 22:33:06 -0400

    Added search, _searc_subfolders, _if_string functions

--- po_py/pyData/path_tools.py
+++ po_py/pyData/path_tools.py
@@ -1,9 +1,10 @@
 """Tools for path-related operations."""
 
-import pathlib as pl
-import input_tools as it
 import re
 import sys
+import glob
+import pathlib as pl
+import input_tools as it
 
 
 def create_path(folder, file, suffix=None):
@@ -29,50 +30,85 @@ def create_path(folder, file, suffix=None):
     else:
         return path
 
-    def prompt_if_exists(path):
-        """Ask user for action of output file name already exists.
 
-        Positional arguments:
-        :path rstr or Path
-        """
-        # If user enters raw string, create the Path object
-        if type(path) == str:
-            path = pl.Path(path)
-        # If the file already exists, ask user for action
-        if path.is_file():
-            print('')
-            choice = it.custom_question('File already exists. ' +
-                                        'What should we do',
-                                        {'Cancel': 'c',
-                                         'Owerwrite': 'o',
-                                         'Increment': 'i'})
-            if choice in ['Increment', 'i']:
-                return uniquify(path)
-            # elif choice in ('change', 'c'):
-            #    new_name = input('What new name?')
-            if choice in ['Owerwrite', 'o']:
-                return path
-            else:
-                sys.exit('Execution cancelled by YOU!!!')
-        else:
+def prompt_if_exists(path):
+    """Ask user for action of output file name already exists.
+
+    Positional arguments:
+    :path rstr or Path
+    """
+    # If user enters raw string, create the Path object
+    path = _if_string(path)
+    # If the file already exists, ask user for action
+    if path.is_file():
+        print('')
+        choice = it.custom_question('File already exists. ' +
+                                    'What should we do',
+                                    {'Cancel': 'c',
+                                     'Owerwrite': 'o',
+                                     'Increment': 'i'})
+        if choice in ['Increment', 'i']:
+            return uniquify(path)
+        # elif choice in ('change', 'c'):
+        #    new_name = input('What new name?')
+        if choice in ['Owerwrite', 'o']:
             return path
-
-    def uniquify(path, sep='_'):
-        """Increment file name if exists.
-
-        Positional arguments:
-        :path raw str or Path
-        """
-        if type(path) == str:
-            path = pl.Path(path)
-        folder = path.parent
-        name = path.stem
-        suffix = ''.join(path.suffixes)
-        s = re.search(re.escape(sep) + '[0-9]+$', name)
-        if s:
-            s = s.start() + 1
-            return create_path(folder, name[:s] +
-                               str(int(name[s:]) + 1).zfill(len(name)-s),
-                               suffix)
         else:
-            return create_path(folder, name + sep + '01', suffix)
+            sys.exit('Execution cancelled by YOU!!!')
+    else:
+        return path
+
+
+def uniquify(path, sep='_'):
+    """Increment file name if exists.
+
+    Positional arguments:
+    :path raw str or Path
+    """
+    path = _if_string(path)
+    folder = path.parent
+    name = path.stem
+    suffix = ''.join(path.suffixes)
+    s = re.search(re.escape(sep) + '[0-9]+$', name)
+    if s:
+        s = s.start() + 1
+        return create_path(folder, name[:s] +
+                           str(int(name[s:]) + 1).zfill(len(name)-s),
+                           suffix)
+    else:
+        return create_path(folder, name + sep + '01', suffix)
+
+
+def search(path, sub=False):
+    """Get occurences of a file name within a directory and sub-directories.
+
+    Positional arguments:
+    :path str or path
+
+    Keyword arguments:
+    :sub boolean
+    """
+    path = _if_string(path)
+    path = _search_subfolders(path, sub)
+    return glob.glob(str(path), recursive=True)
+
+
+def _search_subfolders(path, subfolders):
+    if subfolders:
+        folder = path.parent
+        name = path.name
+        return folder / '**' / name
+    else:
+        return path
+
+
+def _if_string(path):
+    """Transform string argument to Path object.
+
+    Positinal arguments:
+    :path str or Path
+    """
+    if type(path) == str:
+        return pl.Path(path)
+    else:
+        return path
