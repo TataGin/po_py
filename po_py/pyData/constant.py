50624fcde5d90435d9f64e2009614a85f3aab00b
fginez <fginez@wisdomtree.com>
2019-07-15 18:10:36 +0100

    Added crypto DB to the db connection tool

---
+++ po_py/pyData/constant.py
@@ -0,0 +1,10 @@
+"""Contains constants to be used in the various modules of the librairy."""
+
+# Constants for sql_tools
+DEFAULT_SERVER = 'D01'
+
+D01_IP = '10.0.10.199'
+D01_DEFAULT_DB = 'EUDevDB'
+
+P13_IP = '10.0.10.197'
+P13_DEFAULT_DB = 'FundamentalsDB'
--- po_py/pyData/sql_tools.py
+++ po_py/pyData/sql_tools.py
@@ -4,18 +4,41 @@ from getpass import getpass
 import sqlalchemy as sql
 
 
-def connect(db='D01', dialect='mssql', dbapi='pyodbc',
+def connect(db_name='D01', dialect='mssql', dbapi='pyodbc',
             driver='ODBC+Driver+13+for+SQL+Server',
-            port='1433', dbname='EUDevDB'):
+            port='1433'):
+    """Creates engine for interaction with the database.
 
-    if db == 'D01':
-        hostname = '10.0.10.199'
-    elif db == 'P13':
-        hostname = '10.0.10.197'
+    Keyword arguments:
+    ------------------
+    db_name : str
+        The "nickname" of the database
+    dialect : str
+    dbapi : str
+    driver : str
+    port : str
+    """
+    db_name = db_name.strip().lower()
+
+    if db_name == 'd01':
+        host = '10.0.10.199'
+        db = 'EUDevDB'
+
+    elif db_name == 'p13':
+        host = '10.0.10.197'
+        db = 'FundamentalsDB'
+
+    elif db_name == 'crypto':
+        host = 'crypto-market-data-test.cwlmdysgd5t3.eu-west-1.rds.amazonaws.com'
+        db = 'crypto_market_data'
+        dialect = 'mysql'
+        dbapi='pymysql'
+        port='3306'
+        driver = ''
 
     username = input('Username: ')
 
     password = getpass(prompt='Password: ')
 
     return sql.create_engine(dialect+'+'+dbapi+'://'+username+':'+password+'@'
-                             + hostname+':'+port+'/'+dbname+'?driver='+driver)
+                             +host+':'+port+'/'+db+'?driver='+driver)
